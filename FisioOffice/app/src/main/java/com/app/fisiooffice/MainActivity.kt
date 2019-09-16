f93f1cb085b62dfd3928c092d6d47d0f8f71c7fc
joseedsonamorim <37834114+joseedsonamorim@users.noreply.github.com>
2019-09-15 23:14:33 -0300

    primeiro commit

---
+++ FisioOffice/app/src/main/java/com/app/fisiooffice/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.app.fisiooffice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+    }
+}
