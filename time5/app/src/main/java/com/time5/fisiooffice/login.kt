160eeb78686e46c7f4e9978af47acb0b81d03b81
joseedsonamorim <37834114+joseedsonamorim@users.noreply.github.com>
2019-09-18 20:01:43 -0300

    18/09/19

---
+++ time5/app/src/main/java/com/time5/fisiooffice/login.kt
@@ -0,0 +1,21 @@
+package com.time5.fisiooffice
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import fisiooffice.time5.fisiooffice.MainActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+    }
+}
---
+++ time5/app/src/main/java/fisiooffice/time5/fisiooffice/model/Post.kt
@@ -0,0 +1,3 @@
+package fisiooffice.time5.fisiooffice.model
+
+data class Post(val exercicio: String, val text: String, val photo: String)
